1b4e2f59c91215ce51022d08be306dae67a36282
hamid.b.1362 <hamid.b.1362@gmail.com>
2025-01-22 21:35:58 +0330

    Add JwtSecurity to project

---
+++ MiniApi_AuthUser.Application/Service/Implementation/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MiniApi_AuthUser.Application.Service.Interface;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MiniApi_AuthUser.Application.Service.Implementation
+{
+    public class TokenService : ITokenService
+    {
+        private SymmetricSecurityKey _key;
+        public TokenService()
+        {
+            string secrtKey = "kdnksdffksdjfdsfsdfsdfsdf";
+            byte[] data = Encoding.UTF8.GetBytes(secrtKey);
+
+            _key = new SymmetricSecurityKey(data);
+
+        }
+        public string GetToken()
+        {
+            var claim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,"1")
+            };
+            var SigningCredential = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescription = new SecurityTokenDescriptor()
+            {
+                Subject= new ClaimsIdentity(claim),
+                Expires= DateTime.Now.AddDays(1),
+                SigningCredentials= SigningCredential
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token=tokenHandler.CreateToken(tokenDescription);
+            
+            return  tokenHandler.WriteToken(token);
+        }
+    }
+}
+
